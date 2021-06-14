dc9b5195aa35c55cf3b1436dba84423c19840585
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-06-14 17:08:27 -0400

    commit tree 107683561700a1b31665b6c3465c5739cc72d377

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react';
+
+class CoordinatesButton extends Component{
+    onRecieveCoordinates = (event) => {
+        let mouseCoordinates = [event.clientX, event.clientY]
+        console.log(mouseCoordinates)
+        
+    }
+    render(){
+        return(
+            <div>
+                <button onClick={this.props.onRecieveCoordinates(mouseCoordinates)}>Coordinates</button>
+            </div>
+        )
+    }
+}
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,13 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react';
+
+class DelayedButton extends Component{
+    render(){
+        return(
+            <div>
+                <button>Delayed</button>
+            </div>
+        )
+    }
+}
+export default DelayedButton;
